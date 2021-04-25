e8660fb02dee87a41bc9c289248f7a59f09004ed
Rutik <71027989+Rutikab12@users.noreply.github.com>
2021-04-25 13:27:48 +0530

    Food Order Website PHP Website

---
+++ Food Order PHP/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!----footer Section starts---->
+<div class="footer">
+    <div class="wrapper">
+            <p class="text-center">2021 All Rights Reserved, Food House Made with ❤️ By: <a href="#">Rutik Bhoyar</a></p>
+    </div>
+</div>
+    <!----Footer Section Ends---->
+
+</body>
+</html>
